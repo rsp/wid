8b6791eb5a1b474780ba74dbcc7564df34230ce4
Rafał Pocztarski <r.pocztarski@gmail.com>
2019-10-23 07:24:36 +0200

    Update server

--- server.ts
+++ server.ts
@@ -1,12 +1,16 @@
 import { serve } from 'https://deno.land/std@v0.21.0/http/server.ts';
-const encoder = new TextEncoder();
-const server = serve(':8000');
-window.onload = async () => {
-  console.log('Listening on http://localhost:8000/');
+
+(async () => {
+
   let n = 0;
+  const encoder = new TextEncoder();
+  const server = serve(':8000');
+  console.log('Listening on http://localhost:8000/');
+
   for await (const req of server) {
     const message = `Hello #${++n}, Deno Warsaw!\n`;
-    const body = encoder.encode(message);
-    req.respond({ body });
+    req.respond({ body: encoder.encode(message) });
   }
-};
+
+})();
+
